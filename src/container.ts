6b61a0e985008ad13cf59a1fdae173374f3ef0a1
Yanis HADID <yanis5005@gmail.com>
2025-01-28 12:51:32 +0100

    suite test e2e et d'integration

--- src/container.ts
+++ src/container.ts
@@ -1,16 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaWebinarRepository } from 'src/webinars/adapters/webinar-repository.prisma';
 import { ChangeSeats } from 'src/webinars/use-cases/change-seats';
+import { OrganizeWebinars } from 'src/webinars/use-cases/organize-webinar';
+import { RealDateGenerator } from 'src/core/adapters/real-date-generator';
+import { RealIdGenerator } from 'src/core/adapters/real-id-generator';
 
 export class AppContainer {
   private prismaClient!: PrismaClient;
   private webinarRepository!: PrismaWebinarRepository;
   private changeSeatsUseCase!: ChangeSeats;
+  private organizeWebinarUseCase!: OrganizeWebinars;
 
   init(prismaClient: PrismaClient) {
     this.prismaClient = prismaClient;
     this.webinarRepository = new PrismaWebinarRepository(this.prismaClient);
     this.changeSeatsUseCase = new ChangeSeats(this.webinarRepository);
+    this.organizeWebinarUseCase = new OrganizeWebinars(
+      this.webinarRepository,
+      new RealIdGenerator(),
+      new RealDateGenerator(),
+    );
   }
 
   getPrismaClient() {
@@ -19,6 +28,10 @@ export class AppContainer {
 
   getChangeSeatsUseCase() {
     return this.changeSeatsUseCase;
+  }
+
+  getOrganizeWebinarUseCase() {
+    return this.organizeWebinarUseCase;
   }
 }
 
